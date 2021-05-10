64e2685e876f812e7a9285d7341daa2f92cb5871
iremtekin <iremtekin67@gmail.com>
2021-05-10 11:27:01 +0300

    minor

--- code_irem/src/add_course.php
+++ code_irem/src/add_course.php
@@ -193,11 +193,11 @@ if(isset($_POST['add_course_button']))
 </g>
 </g></svg><img></span>&nbsp;Invite Instructor
         </a>
-        <a href="invite_instructor.php" class="u-btn u-btn-round u-button-style u-hover-palette-2-dark-1 u-palette-2-light-2 u-radius-25 u-btn-5"><span class="u-icon u-icon-4"><svg class="u-svg-content" viewBox="0 0 512 512" x="0px" y="0px" style="width: 1em; height: 1em;"><g><g><path d="M256,0C114.833,0,0,114.833,0,256s114.833,256,256,256s256-114.853,256-256S397.167,0,256,0z M256,472.341    c-119.275,0-216.341-97.046-216.341-216.341S136.725,39.659,256,39.659S472.341,136.705,472.341,256S375.295,472.341,256,472.341z    "></path>
+        <a href="add_quiz.php" class="u-btn u-btn-round u-button-style u-hover-palette-2-dark-1 u-palette-2-light-2 u-radius-25 u-btn-5"><span class="u-icon u-icon-4"><svg class="u-svg-content" viewBox="0 0 512 512" x="0px" y="0px" style="width: 1em; height: 1em;"><g><g><path d="M256,0C114.833,0,0,114.833,0,256s114.833,256,256,256s256-114.853,256-256S397.167,0,256,0z M256,472.341    c-119.275,0-216.341-97.046-216.341-216.341S136.725,39.659,256,39.659S472.341,136.705,472.341,256S375.295,472.341,256,472.341z    "></path>
 </g>
 </g><g><g><path d="M355.148,234.386H275.83v-79.318c0-10.946-8.864-19.83-19.83-19.83s-19.83,8.884-19.83,19.83v79.318h-79.318    c-10.966,0-19.83,8.884-19.83,19.83s8.864,19.83,19.83,19.83h79.318v79.318c0,10.946,8.864,19.83,19.83,19.83    s19.83-8.884,19.83-19.83v-79.318h79.318c10.966,0,19.83-8.884,19.83-19.83S366.114,234.386,355.148,234.386z"></path>
 </g>
-</g></svg><img></span>&nbsp;Add Quız
+</g></svg><img></span>&nbsp;Add Quiz
         </a>
     </div>
 </section>
