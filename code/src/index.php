ba17601c46fb412ae80d71e37b52f77d396cf058
Ömer Ünlüsoy <omerunlusoy99@icloud.com>
2021-05-10 20:06:03 +0300

    minor

--- code/src/index.php
+++ code/src/index.php
@@ -1,134 +1,155 @@
-<!DOCTYPE html>
-<html lang="en">
-<title>SAPIENTIA</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-</style>
-
-<body>
 <?php
 
-//config inclusion session starts
-include "config.php";
-session_start();
-
-//defining necessary variables
-$username = "";
-$password = "";
+include("connect.php");
 
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // username and password sent from form
-    $username = mysqli_real_escape_string($db, $_POST['e-mail']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-
-    //sql query for checking inputs and finding corresponding student
-    $sql = "SELECT e-mail, password FROM Student WHERE SID = ? and name = ?";
-    if($stmt = mysqli_prepare($db, $sql)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "ss", $entered_username, $entered_password);
-
-        //set parameters
-        $entered_username = $username;
-        $entered_password = $password;
-
-        //executing the statement
-        if(mysqli_stmt_execute($stmt)){
-            mysqli_stmt_store_result($stmt);
-            //checking if sid and sname is true
-            if(mysqli_stmt_num_rows($stmt) == 1){
-                mysqli_stmt_bind_result($stmt, $username, $turned_pw);
-
-                if(mysqli_stmt_fetch($stmt)){
-                    if($turned_pw == $password){
-                        //inputs are correct session is starting
-                        session_start();
-                        $_SESSION['sname'] = $username;
-                        $_SESSION['sid'] = $password;
-                        header("location: student_main_page.php");
-                    }
-                }
-            }else{
-                //wrong input
-                echo "<script type='text/javascript'>alert('Invalid E-mail or Password.');</script>";
-            }
-
-        }
-    }
-
-    // Close statement
-    mysqli_stmt_close($stmt);
-}
 ?>
 
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Learn Everyday, Join online courses today, Train Your Brain Today!, Learn to enjoyevery minute of your life., Online Learning, Innovations in Online Learning, Education and Learning, 01, 02, 03, 04, Contact Us">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Home</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.13.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i|Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
 
 
-<!-- Header -->
-<header class="w3-container w3-cyan w3-center" style="padding:12px 16px">
-    <h1 class="w3-margin w3-jumbo">Sapientia<br> Online Course Platform</h1>
-</header>
 
-<div class="w3-row-padding w3-padding-64 w3-container">
-    <div class="w3-content">
-        <div class="w3-twothird">
-            <h1>Login</h1>
-            <h2 class="w3-padding-32">
-
-
-                <form id="loginForm" action="" method="post">
-                    <div class="form-group">
-                        <label for="e-mail">E-mail</label> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                        <input type="text" name="e-mail" class="form-control" id="e-mail" placeholder="Your e-mail..">
-
+    <script type="application/ld+json">{
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "name": "",
+            "url": "index.html",
+            "logo": "images/SapientiaLogo.PNG"
+        }</script>
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#478ac9">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+</head>
+<body data-home-page="Home.html" data-home-page-title="Home" class="u-body"><header class="u-clearfix u-header u-header" id="sec-85c8"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="521" data-image-height="202">
+            <img src="images/SapientiaLogo.PNG" class="u-logo-image u-logo-image-1" data-image-width="196.129">
+        </a>
+    </div></header>
+<section class="u-clearfix u-image u-valign-top u-section-1" id="sec-832e">
+    <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+        <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+                <div class="u-align-left u-container-style u-layout-cell u-size-29 u-layout-cell-1">
+                    <div class="u-container-layout u-container-layout-1">
+                        <h5 class="u-custom-font u-font-pt-sans u-text u-text-1">online learning</h5>
+                        <h1 class="u-text u-text-2">Sapientia</h1>
+                        <p class="u-text u-text-3">Sapientia is what you need in life.<br>
+                            <br>
+                            <br>
+                        </p>
+                        <form method="POST" action="signUp.php">
+                            <a   id="signUpButton" class="u-active-palette-1-dark-2 u-btn u-btn-rectangle u-button-style u-custom-font u-font-pt-sans u-hover-palette-1-dark-2 u-palette-1-dark-3 u-radius-0 u-btn-1">Sign up for Wısdom</a>
+                            <script type="text/javascript">
+                                document.getElementById("signUpButton").onclick = function () {
+                                    location.href = "signUp.php";
+                                };
+                            </script>
+                        </form>
+                        <form method="POST" action="login.php">
+                            <a id="loginButton"  class="u-active-palette-1-dark-2 u-btn u-btn-rectangle u-button-style u-custom-font u-font-pt-sans u-hover-palette-1-dark-2 u-palette-1-dark-3 u-radius-0 u-btn-2">logın for Wısdom</a>
+                            <script type="text/javascript">
+                                document.getElementById("loginButton").onclick = function () {
+                                    location.href = "login.php";
+                                };
+                            </script>
+                        </form>
                     </div>
-                    <br>
-                    <div class="form-group">
-                        <label for="password">Password</label> &nbsp; &nbsp;
-                        <input type="password" name="password" class="form-control" id="password" placeholder="Your password..">
-
-                    </div>
-                    <br>
-                    <div
-                    <a onclick="tryLogin()" class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Login</a>
+                </div>
+                <div class="u-align-right u-container-style u-image u-layout-cell u-size-31 u-image-1">
+                    <div class="u-container-layout u-valign-bottom u-container-layout-2"></div>
+                </div>
+            </div>
         </div>
-                    <div
-                    <a onclick="signup()" class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Sign up</a>
-        </div>
-        </form>
-        </h2>
-
-        <p class="w3-text-grey"></p>
+    </div>
+</section>
+<section class="u-align-center u-clearfix u-section-2" id="sec-4fce">
+    <form action="#" method="get" class="u-border-1 u-border-grey-30 u-search u-search-left u-white u-search-1">
+        <button class="u-search-button" type="submit">
+          <span class="u-search-icon u-spacing-10">
+            <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 56.966 56.966"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-edc1"></use></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-edc1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" class="u-svg-content"><path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path></svg>
+          </span>
+        </button>
+        <input class="u-search-input" type="search" name="search" value="" placeholder="Search">
+    </form>
+    <div class="u-table u-table-responsive u-table-1">
+        <table class="u-table-entity u-table-entity-1">
+            <colgroup>
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+                <col width="20%">
+            </colgroup>
+            <thead class="u-palette-4-base u-table-header u-table-header-1">
+            <tr style="height: 49px;">
+                <th class="u-border-1 u-border-palette-4-base u-table-cell">Course Name</th>
+                <th class="u-border-1 u-border-palette-4-base u-table-cell">Price</th>
+                <th class="u-border-1 u-border-palette-4-base u-table-cell">Level</th>
+                <th class="u-border-1 u-border-palette-4-base u-table-cell">Category</th>
+                <th class="u-border-1 u-border-palette-4-base u-table-cell">Instructor</th>
+            </tr>
+            </thead>
+            <tbody class="u-table-body">
+            <tr style="height: 67px;">
+                <td class="u-border-1 u-border-grey-30 u-first-column u-grey-5 u-table-cell u-table-cell-6">Row 1</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell"></td>
+            </tr>
+            <tr style="height: 69px;">
+                <td class="u-border-1 u-border-grey-30 u-first-column u-grey-5 u-table-cell u-table-cell-11">Row 2</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell"></td>
+            </tr>
+            <tr style="height: 69px;">
+                <td class="u-border-1 u-border-grey-30 u-first-column u-grey-5 u-table-cell u-table-cell-16">Row 3</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell"></td>
+            </tr>
+            <tr style="height: 69px;">
+                <td class="u-border-1 u-border-grey-30 u-first-column u-grey-5 u-table-cell u-table-cell-21">Row 4</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell">Description</td>
+                <td class="u-border-1 u-border-grey-30 u-table-cell"></td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 
-    <div class="w3-third w3-center">
+    <a href="https://nicepage.com/c/table-html-templates" class="u-btn u-button-style u-btn-5">View</a>
+    <a href="https://nicepage.com/c/table-html-templates" class="u-btn u-button-style u-btn-6">View</a>
+    <a href="https://nicepage.com/c/table-html-templates" class="u-btn u-button-style u-btn-7">View</a>
+    <a href="https://nicepage.com/c/table-html-templates" class="u-btn u-button-style u-btn-8">View</a>
+</section>
 
-    </div>
-</div>
-</div>
 
+<footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-266b"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1">Wisdom is life...</p>
+    </div></footer>
 
-<script type="text/javascript">
-    function tryLogin() {
-        var email_value = document.getElementById("e-mail").value;
-        var password_value = document.getElementById("password").value;
-        if (email_value === "" || password_value === "") {
-            alert("Fill e-mail and password fields.");
-        }
-        else {
-            var form = document.getElementById("loginForm").submit();
-        }
-    }
-
-    function signup() {
-        window.location.href='create_account.php';
-    }
-</script>
 </body>
 </html>
--- code/src/logout.php
+++ code/src/logout.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
-$_SESSION = array();
 session_destroy();
-
-// Redirect to login page
-echo "<script LANGUAGE='JavaScript'>
-          window.alert('Logged Out');
-          window.location.href='index.php';
-       </script>";
-exit;
-?>
+header("Location:index.php");
+exit();
